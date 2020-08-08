ec50b0dd7efbb9f1242ddab172b4bbb0134c8683
ExtraCold <extracold1209@gmail.com>
2020-08-09 08:36:14 +0900

    mainContainer 의 card responsive css 래핑쪽 리팩토링

--- src/components/templates/mainContainer.tsx
+++ src/components/templates/mainContainer.tsx
@@ -1,15 +1,13 @@
 import React, {useCallback, useState} from 'react';
-import styled from 'styled-components';
 import TimerCard from '../atoms/TimerCard';
 import {Col, Container, Row} from 'react-grid-system';
 import AddTimerCard from '../atoms/AddTimerCard';
 
-const CenterContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
+const CardWrapper: React.FC = ({children, ...props}) => (
+    <Col xs={12} md={6} lg={4} xl={3} {...props}>
+        {children}
+    </Col>
+);
 
 const MainContainer: React.FC = () => {
     const [dummyDatum, setDummyDatum] = useState([10, 15, 20, 25]);
@@ -25,14 +23,14 @@ const MainContainer: React.FC = () => {
                 <Row>
                     {
                         dummyDatum.map((data, index) => (
-                            <Col xs={12} md={6} lg={4} xl={3} key={`card-${index}`}>
+                            <CardWrapper key={`card-${index}`}>
                                 <TimerCard initialTime={data}/>
-                            </Col>
+                            </CardWrapper>
                         ))
                     }
-                    <Col xs={12} md={6} lg={4} xl={3}>
+                    <CardWrapper>
                         <AddTimerCard onClick={handleAddTimerClicked}/>
-                    </Col>
+                    </CardWrapper>
                 </Row>
             </Container>
         </>
