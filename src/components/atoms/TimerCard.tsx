a971b32ccd2b133a12e5b844ec52962d3142322b
ExtraCold <extracold1209@gmail.com>
2020-08-08 16:39:28 +0900

    우측 상단에 설정변경 / 삭제 아이콘 추가

--- src/components/atoms/TimerCard.tsx
+++ src/components/atoms/TimerCard.tsx
@@ -3,6 +3,7 @@ import addSeconds from 'date-fns/addSeconds';
 import formatDate from 'date-fns/format';
 import styled from 'styled-components';
 import useTimer from '../../hooks/useTimer';
+import {SettingsIcon, XCircleIcon} from '@primer/octicons-react';
 
 
 const Container = styled.div`
@@ -13,6 +14,17 @@ const Container = styled.div`
     padding: 20px;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     border-radius: 5px;
+`;
+
+const Header = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const IconWrapper = styled.div`
+    opacity: 0.7;
+    margin-left: 5px;
 `;
 
 const TimerNameSpan = styled.span`
@@ -51,6 +63,10 @@ const TimerCard: React.FC = (props) => {
 
     return (
         <Container>
+            <Header>
+                <IconWrapper><SettingsIcon size={24}/></IconWrapper>
+                <IconWrapper><XCircleIcon size={24}/></IconWrapper>
+            </Header>
             <TimerNameSpan>이름따리</TimerNameSpan>
             <IndicatorContainer>
                 {convertSecondToDisplayText(remainSeconds)}
--- src/components/templates/mainContainer.tsx
+++ src/components/templates/mainContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TimerCard from '../atoms/TimerCard';
+import {Col, Container, Row} from 'react-grid-system';
 
 const CenterContainer = styled.div`
     display: flex;
@@ -12,10 +13,26 @@ const CenterContainer = styled.div`
 const MainContainer: React.FC = () => {
 
     return (
-        <CenterContainer>
+        <>
             <div>Hello MainContainer</div>
-            <TimerCard />
-        </CenterContainer>
+            <Container fluid>
+                <Row>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <TimerCard />
+                    </Col>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <TimerCard />
+                    </Col>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <TimerCard />
+                    </Col>
+                    <Col xs={12} md={6} lg={4} xl={3}>
+                        <TimerCard />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+
     );
 };
 
