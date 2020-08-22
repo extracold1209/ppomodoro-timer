685c63f2a2b722da6fad29d3538e9dfd566d8694
extracold1209 <extracold1209@gmail.com>
2020-08-22 17:22:09 +0900

    [feature] 각 FlattenCard 컴포넌트가 Timer 를 가지도록 구현
    - useTimer 적용
    - TODO : initialSecond 가 바뀔때 타이머가 정지하고 값이 바뀔 수 있어야함

--- src/components/atoms/FlattenCard.tsx
+++ src/components/atoms/FlattenCard.tsx
@@ -1,19 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
+import useTimer from '../../hooks/useTimer';
 
 const CardContainer = styled.div`
     width: 100%;
     display: flex;
+    flex-direction: column;
     padding: 10px 5px;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     border-radius: 5px;
     background-color: white;
 `;
 
+const HeaderContainer = styled.div`
+    align-content: space-between;
+    display: flex;
+    min-height: 10vh;
+`;
+
+const HeaderButtonContainer = styled.div`
+    display: flex;
+    position: absolute;
+    right: 0;
+    padding-right: 20px;
+`;
+
+const HeaderTitleContainer = styled.div`
+    display: flex;
+`;
+
+const ContentContainer = styled.div`
+    display: flex;
+`;
+
 const FlattenCard: React.FC<{ timer: Timer }> = (props) => {
+    const {timer} = props;
+    const {title, initialSecond} = timer;
+    const [start, stop, remainSeconds] = useTimer(initialSecond);
+
     return (
         <CardContainer>
-            Hello Card
+            <HeaderContainer>
+                <HeaderTitleContainer>
+                    {title || '타이틀이 없어욧'}
+                </HeaderTitleContainer>
+                <HeaderButtonContainer>
+                    <button onClick={() => {
+                        start();
+                    }}>실행
+                    </button>
+                    <button onClick={() => {
+                        stop();
+                    }}>정지
+                    </button>
+                </HeaderButtonContainer>
+            </HeaderContainer>
+            <ContentContainer>
+                {remainSeconds} / {initialSecond}
+            </ContentContainer>
         </CardContainer>
     );
 };
