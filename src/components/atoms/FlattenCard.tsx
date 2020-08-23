256f938f0e814abfe4396354c77cbbb2c5f1a1ed
extracold1209 <extracold1209@gmail.com>
2020-08-23 15:54:37 +0900

    [chore] 버튼 간격 띄우고 폰트 크기 조정

--- src/components/atoms/FlattenCard.tsx
+++ src/components/atoms/FlattenCard.tsx
@@ -1,13 +1,15 @@
 import React, {useCallback, useState} from 'react';
 import styled from 'styled-components';
 import useTimer from '../../hooks/useTimer';
-import {BiPlayCircle, BiStopCircle, BiXCircle} from 'react-icons/bi/index';
+import {BiPlayCircle, BiStopCircle, BiXCircle, BiCog} from 'react-icons/bi/index';
 
 const CardContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
-    padding: 10px 5px;
+    justify-content: center;
+    align-items: center;
+    padding: 25px 15px;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     border-radius: 5px;
     background-color: white;
@@ -17,13 +19,20 @@ const HeaderContainer = styled.div`
     align-content: space-between;
     display: flex;
     min-height: 10vh;
+    font-size: 18px;
 `;
 
-const HeaderButtonContainer = styled.div`
+const HeaderIconContainer = styled.div`
     display: flex;
     position: absolute;
+    top: 0;
     right: 0;
-    padding-right: 20px;
+    padding-top: 10px;
+    padding-right: 10px;
+    
+    > *:not(:last-child) {
+        margin-right: 5px;
+    }
 `;
 
 const HeaderTitleContainer = styled.div`
@@ -32,6 +41,7 @@ const HeaderTitleContainer = styled.div`
 
 const ContentContainer = styled.div`
     display: flex;
+    font-size: 32px;
 `;
 
 const FlattenCard: React.FC<{ timer: Timer, onDelete?: () => void }> = (props) => {
@@ -60,14 +70,15 @@ const FlattenCard: React.FC<{ timer: Timer, onDelete?: () => void }> = (props) =
                     <HeaderTitleContainer>
                         {title || '타이틀이 없어욧'}
                     </HeaderTitleContainer>
-                    <HeaderButtonContainer>
+                    <HeaderIconContainer>
+                        <BiCog size={24}/>
                         {
                             isTimerRunning
                                 ? <BiStopCircle size={24} onClick={handleStopButtonClicked}/>
                                 : <BiPlayCircle size={24} onClick={handlePlayButtonClicked}/>
                         }
                         <BiXCircle size={24} onClick={handleDeleteButtonClicked}/>
-                    </HeaderButtonContainer>
+                    </HeaderIconContainer>
                 </HeaderContainer>
                 <ContentContainer>
                     {remainSeconds} / {initialSecond}
