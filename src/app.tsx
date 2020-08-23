c4cc5b0364568edb4742255a27e4c761fc536898
extracold1209 <extracold1209@gmail.com>
2020-08-23 16:18:16 +0900

    [feature] setting modal 기본 구조 추가

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './stores';
+import Modal from 'react-modal';
 import CarouselContainer from './components/templates/CarouselContainer';
 
+const rootElement = document.getElementById('__workspace__') as HTMLElement;
+Modal.setAppElement(rootElement);
 ReactDom.render(
     <Provider store={store}>
         <CarouselContainer />
     </Provider>
-    , document.getElementById('__workspace__'));
+    , rootElement);
