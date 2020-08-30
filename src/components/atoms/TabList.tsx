36eac47cc6d4bc705415eafe79868da60d164cf8
ExtraCold <extracold1209@gmail.com>
2020-08-30 15:09:56 +0900

    [chore] select, hover 배경 외부 props 받을 수 있도록 수정

--- src/components/atoms/TabList.tsx
+++ src/components/atoms/TabList.tsx
@@ -5,33 +5,35 @@ import styled from '@emotion/styled';
 type Props = {
     listItems: string[];
     onClick?: (item: string, activated: boolean) => void;
+    hoverBackground?: string;
+    selectBackground?: string;
 }
 
-const ItemTab = styled<typeof Box, {isActivate: boolean}>(Box)`
+const ItemTab = styled<typeof Box, {
+    isActivate: boolean
+    hoverBackground: string;
+    selectBackground: string;
+}>(Box)`
     cursor: pointer;
     margin: 0 4px;
     padding: 12px;
-    background-color: ${({isActivate}) => isActivate ? 'black' : 'transparent'};
+    background-color: ${({isActivate, selectBackground}) => isActivate ? selectBackground : 'transparent'};
     color: ${({isActivate}) => isActivate ? 'white' : 'black'};
     
     &:hover {
-        background-color: darkseagreen;
+        background-color: ${({hoverBackground}) => hoverBackground};
         color: white;
     }
 `;
 
-const TabList: React.FC<Props> = ({listItems, onClick}) => {
+const TabList: React.FC<Props> = ({listItems, onClick, hoverBackground= 'darkseagreen', selectBackground = 'black'}) => {
     const [currentSelected, setCurrentSelected] = useState<string | undefined>(undefined);
     const handleTabClicked = useCallback((item: string) => () => {
         // 만약 이미 선택된 탭을 다시 누르면 deactivate 된다.
         const isActivateSelect = !currentSelected || currentSelected !== item;
 
         onClick?.(item, isActivateSelect);
-        if (isActivateSelect) {
-            setCurrentSelected(item);
-        } else {
-            setCurrentSelected(undefined);
-        }
+        setCurrentSelected(isActivateSelect ? item : undefined);
     }, [currentSelected]);
 
     return (
@@ -42,6 +44,8 @@ const TabList: React.FC<Props> = ({listItems, onClick}) => {
                         key={item}
                         onClick={handleTabClicked(item)}
                         isActivate={item === currentSelected}
+                        hoverBackground={hoverBackground}
+                        selectBackground={selectBackground}
                     >
                         {item}
                     </ItemTab>
