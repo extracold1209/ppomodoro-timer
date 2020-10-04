7dc153246d055a3e69a08ace8682a58319b5ad27
extracold <extracold1209@gmail.com>
2020-10-04 16:10:07 +0900

    [chore] webpack 설정이 먼저 NODE_ENV 를 보도록 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: process.env.NODE_ENV || 'development',
     entry: path.join(__dirname, 'src', 'app.tsx'),
     output: {
         path: path.join(__dirname, 'public', 'dist'),
