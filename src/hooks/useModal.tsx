65aead038b78174f0778efc9431c239136da4837
extracold <extracold1209@gmail.com>
2020-10-17 00:03:46 +0900

    [chore] modal container style 디자인 개선

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -22,8 +22,12 @@ export default (props: IProps): [VisibleFunction, Component] => {
             onRequestClose={() => setOpenState(false)}
             style={{
                 content: {
+                    maxWidth: 500,
+                    margin: 'auto',
                     padding: 0,
                     border: 'none',
+                    borderRadius: 4,
+                    boxShadow: 'rgba(0, 0, 0, 0.5) 10px 10px 10px 0px',
                 },
                 overlay: {
                     backgroundColor: 'rgba(255, 255, 255, 0.2)'
